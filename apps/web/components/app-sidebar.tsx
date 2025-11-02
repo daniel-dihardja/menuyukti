6232820b4965e4dc9f988d990ce9f11135ff8681
Daniel Dihardja <d.dihardja@house-of-communication-external.com>
2025-11-02 14:35:53 +0100

    add brand logo placeholder

--- apps/web/components/app-sidebar.tsx
+++ apps/web/components/app-sidebar.tsx
@@ -1,12 +1,29 @@
 "use client";
 
 import * as React from "react";
-import { Sidebar, SidebarContent } from "@workspace/ui/components/sidebar";
+import {
+  Sidebar,
+  SidebarHeader,
+  SidebarContent,
+} from "@workspace/ui/components/sidebar";
 import { NavMain } from "./nav-main";
+import { LayoutDashboard } from "lucide-react";
+import Link from "next/link";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader className="relative h-16 border-b">
+        <Link
+          href="/"
+          className="
+            absolute left-1 top-6 flex items-center gap-2 pl-3
+          "
+        >
+          <LayoutDashboard className="w-4 h-4 text-muted-foreground" />
+        </Link>
+      </SidebarHeader>
+
       <SidebarContent>
         <NavMain />
       </SidebarContent>
