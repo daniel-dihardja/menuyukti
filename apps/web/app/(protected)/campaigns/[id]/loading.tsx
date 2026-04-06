f0605ccc462ebf26d00f1897a81150d6709f38a3
Daniel Dihardja <daniel@dihardja.de>
2026-04-06 22:39:43 +0200

    fix loading style

--- apps/web/app/(protected)/campaigns/[id]/loading.tsx
+++ apps/web/app/(protected)/campaigns/[id]/loading.tsx
@@ -1,13 +1,26 @@
+import { getTranslations } from 'next-intl/server'
+import { AnalyticsPageShell } from '@/components/analytics-page-shell'
+import { routes } from '@/lib/routes'
 import { Skeleton } from '@workspace/ui/components/skeleton'
 
-export default function CampaignDetailLoading() {
+export default async function CampaignDetailLoading() {
+  const tCampaigns = await getTranslations('analytics.campaigns')
+  const tChat = await getTranslations('analytics.campaigns.chat')
+  const title = tChat('pageLoadingTitle')
+
   return (
-    <div className="flex h-[calc(100vh-4rem)] min-h-[24rem] flex-col gap-4 p-4">
-      <Skeleton className="h-8 w-48" />
-      <div className="grid flex-1 grid-cols-3 gap-4">
-        <Skeleton className="col-span-1 h-full min-h-[12rem] rounded-lg" />
-        <Skeleton className="col-span-2 h-full min-h-[12rem] rounded-lg" />
+    <AnalyticsPageShell
+      title={title}
+      breadcrumbs={[{ label: tCampaigns('title'), href: routes.campaigns.list }, { label: title }]}
+      mainClassName="max-w-none flex min-h-0 min-h-[24rem] w-full flex-1 flex-col"
+    >
+      <div className="flex min-h-0 min-w-0 flex-1 flex-col gap-4">
+        <Skeleton className="h-8 w-48 max-w-full" />
+        <div className="grid min-h-0 min-w-0 flex-1 grid-cols-3 gap-4">
+          <Skeleton className="col-span-1 h-full min-h-[12rem] rounded-lg" />
+          <Skeleton className="col-span-2 h-full min-h-[12rem] rounded-lg" />
+        </div>
       </div>
-    </div>
+    </AnalyticsPageShell>
   )
 }
