e6cb9040b7b6ad451c7d92e5eb835631f9243416
Daniel Dihardja <daniel@dihardja.de>
2026-04-20 11:19:44 +0200

    fix import ordering

--- apps/graphql/schema/queries/__init__.py
+++ apps/graphql/schema/queries/__init__.py
@@ -11,8 +11,8 @@ from .menu_heatmaps import MenuHeatmapsQuery
 from .milestone_prior_data import MilestonePriorDataQuery
 from .nodes import NodesQuery
 from .operating_profile import OperatingProfileQuery
-from .promotion_menu_items import PromotionMenuItemsQuery
 from .promotion_candidates_signals import PromotionCandidatesSignalsQuery
+from .promotion_menu_items import PromotionMenuItemsQuery
 from .public_holidays import PublicHolidaysQuery
 from .revenue_trends import RevenueTrendsQuery
 from .weekly_demand_pattern import WeeklyDemandPatternQuery
