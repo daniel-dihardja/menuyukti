c4c31ef411ee4aba1617a5e8ac3e875c2b8fdf75
Daniel Dihardja <daniel@dihardja.de>
2026-09-19 10:20:32 +0200

    fix format

--- apps/web/app/(protected)/analytics/locations/[id]/menu/location-menu-form.tsx
+++ apps/web/app/(protected)/analytics/locations/[id]/menu/location-menu-form.tsx
@@ -380,10 +380,7 @@ function LocationMenuItemRow({
 
                 <div className="flex flex-col gap-2">
                   {group.options.map((option) => (
-                    <div
-                      key={option.key}
-                      className="flex flex-col gap-2 sm:flex-row sm:items-end"
-                    >
+                    <div key={option.key} className="flex flex-col gap-2 sm:flex-row sm:items-end">
                       <Field className="min-w-0 flex-1">
                         <FieldLabel htmlFor={`mod-opt-${option.key}`}>
                           {t('fields.modifierOptionName')}
